d03da279556baa8e3455b62b7328637d3796b620
Kevin <kevinfiorentino.1294@gmail.com>
2023-05-18 16:25:44 -0300

    Anchor setup

---
+++ tests/solana-nft.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolanaNft } from "../target/types/solana_nft";
+
+describe("solana-nft", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.SolanaNft as Program<SolanaNft>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
