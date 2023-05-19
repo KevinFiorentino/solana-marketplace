358a45ea2929ee28b61314f4bfbdc0be3307bcff
Kevin <kevinfiorentino.1294@gmail.com>
2023-05-19 11:39:14 -0300

    Update dependencies, first instruction

--- programs/solana-nft/src/lib.rs
+++ programs/solana-nft/src/lib.rs
@@ -1,22 +1,56 @@
 use {
     anchor_lang::{
-        prelude::*, solana_program::program::invoke, solana_program::program::invoke_signed,
-        system_program, AnchorDeserialize, AnchorSerialize,
+        prelude::*/* , solana_program::program::invoke, solana_program::program::invoke_signed,
+        system_program, AnchorDeserialize, AnchorSerialize, */
     },
-    anchor_spl::{associated_token, token, token::Token},
+    /* anchor_spl::{associated_token, token, token::Token},
     mpl_token_metadata::instruction::{
         approve_collection_authority, create_master_edition_v3, create_metadata_accounts_v3,
         set_and_verify_collection, sign_metadata, update_metadata_accounts_v2,
     },
-    mpl_token_metadata::state::Creator,
+    mpl_token_metadata::state::Creator, */
 };
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GjsR1GVT5G51oMuTDrRzPporaknzWM39TgJs9n84Wmti");
 
 #[program]
 pub mod solana_nft {
     use super::*;
 
+    pub fn create_user_account(
+        ctx: Context<CreateUserAccount>
+    ) -> Result<()> {
+        ctx.accounts.user_pda.bump = *ctx.bumps.get("user_pda").unwrap();
+        ctx.accounts.user_pda.collections_qty = 0;
+
+        msg!("user_pda: {}", *ctx.bumps.get("user_pda").unwrap());
+
+        Ok(())
+    }
+
 }
 
+#[derive(Accounts)]
+pub struct CreateUserAccount<'info> {
+    #[account(
+        init,
+        payer = payer,
+        space = 200,
+        seeds = [
+            b"user_account".as_ref(), payer.to_account_info().key.as_ref()
+        ],
+        bump
+    )]
+    user_pda: Account<'info, UserPDA>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
 
+#[account]
+#[derive(Default)]
+pub struct UserPDA {
+    pub collections_qty: u64,
+    pub bump: u8,
+}
--- tests/solana-nft.ts
+++ tests/solana-nft.ts
@@ -1,16 +1,43 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { SolanaNft } from '../target/types/solana_nft';
 import { expect } from 'chai';
+
+const { SystemProgram } = anchor.web3;
 
 describe('Solana NFT', () => {
 
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.SolanaChat as Program<SolanaNft>;
+  const program = anchor.workspace.SolanaNft as Program<SolanaNft>;
 
-  it('', async () => {
-    
+  const [userPDA] = anchor.web3.PublicKey
+    .findProgramAddressSync(
+      [
+        Buffer.from('user_account'),
+        provider.wallet.publicKey.toBytes()
+      ],
+      program.programId
+    );
+
+  it('Create user', async () => {
+    const tx = await program.methods
+      .createUserAccount()
+      .accounts({
+        userPda: userPDA,
+        rent: SYSVAR_RENT_PUBKEY,
+        payer: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+    console.log('tx', tx);
   });
+
+  it('Get users', async () => {
+    const users = await program.account.userPda.all();
+    console.log('users', users);
+  });
+
 });
